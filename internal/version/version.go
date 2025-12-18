42ed8b10f590e3dcaae62b265569ce74cc2578dc
godofphonk <godofphonk@users.noreply.github.com>
2025-12-19 02:35:55 +0500

    feat: Release v1.1.6.1
    - Update version to 1.1.6.1
    - Production deployment configuration ready
    - HTTP/2 support for worldwide mode
    - All features tested and working

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 // Version information
 var (
 	// Version is the current version of ServerEye
-	Version = "1.1.5.3"
+	Version = "1.1.6.1"
 
 	// BuildDate is set during build time
 	BuildDate = "dev"
