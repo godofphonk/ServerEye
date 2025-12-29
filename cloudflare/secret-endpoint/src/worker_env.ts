d1d8130c3b7103394d7f545673ca12cf6a0fd7dc
godofphonk <godofphonk@users.noreply.github.com>
2025-12-29 05:43:23 +0500

    fix: use postgres user instead of servereye_keys
    - Change PostgreSQL user to default postgres
    - Update all connection strings
    - Fix healthcheck to use postgres user

--- cloudflare/secret-endpoint/src/worker_env.ts
+++
@@ -1,6 +0,0 @@
-export interface Env {
-  SERVEREYE_API_URL: string;
-  USE_WORKER_API?: string;
-  INSTALLER_KEY?: string;
-  SECRET_TOKEN?: string;
-}
--- cloudflare/worker/src/worker_env.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="@cloudflare/workers-types" />
-
-export interface Env {
-  REG_DB: D1Database;
-  INSTALLER_KEY?: string;
-  WEBHOOK_URL?: string;
-  WEBHOOK_SECRET?: string;
-}
