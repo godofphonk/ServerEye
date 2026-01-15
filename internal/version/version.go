2ea6b42b2975367778fed332e8241ee910162f78
godofphonk <godofphonk@users.noreply.github.com>
2026-01-15 07:48:21 +0500

    chore: bump version to 1.1.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 // Version information
 var (
 	// Version is the current version of ServerEye
-	Version = "1.0.0"
+	Version = "1.1.0"
 
 	// BuildDate is set during build time
 	BuildDate = "dev"
